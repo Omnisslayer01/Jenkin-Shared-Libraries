4d450c90129b47159e9d4986c5c3da8d231ad409
Abduldiyan Deshmukh <abduldiyandeshmukh2005@gmail.com>
2026-09-12 00:33:41 +0530

    Enhance docker_push.groovy with safer credential handling
    Updated docker login command to use safer quoting for credentials and added comments for clarity.

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -4,7 +4,9 @@ def call(String username, String image_name, String version){
     usernameVariable:'DockerHubUser',
     passwordVariable:'DockerHubPassword'
   )]){
-  sh 'docker login -u ${DockerHubUser} -p ${DockerHubPassword}'
+  sh 'docker login -u ${DockerHubUser} -p ${DockerHubPassword}'  
+    // When writing credentials this line try to write them in ' ' as it is safer than " ", " " prints the password directly if there is an error, while ' ' doesnt
+    // With ' ' you dont need to write ${env.DockerHubUser} as docker automatically resolves it for you. When using " " you need to write ${env.variable} for env variables
   sh "docker push ${username}/${image_name}:${version}"
   }
 }
