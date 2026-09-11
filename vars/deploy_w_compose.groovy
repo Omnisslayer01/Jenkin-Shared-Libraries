b117fbdce5ba01aac808c949b1440dfb1d7f3e85
Abduldiyan Deshmukh <abduldiyandeshmukh2005@gmail.com>
2026-09-12 00:49:46 +0530

    Update deployment script to restart containers

--- vars/deploy_w_compose.groovy
+++ vars/deploy_w_compose.groovy
@@ -2,5 +2,5 @@ def call(){
   sh "docker stop mysql_db django_cont nginx_cont || true"
   sh "docker rm mysql_db django_cont nginx_cont || true"
 
-  sh "docker compose up -d"
+  sh "docker compose down && docker compose up -d"
 }
