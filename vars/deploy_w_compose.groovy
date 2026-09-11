1f34f3ec4195c236f9aa9a37a6a9aac4f1f5ba9b
Abduldiyan Deshmukh <abduldiyandeshmukh2005@gmail.com>
2026-09-12 00:50:16 +0530

    Comment out docker stop and rm commands
    Comment out docker stop and rm commands for safety.

--- vars/deploy_w_compose.groovy
+++ vars/deploy_w_compose.groovy
@@ -1,6 +1,6 @@
 def call(){
-  sh "docker stop mysql_db django_cont nginx_cont || true"
-  sh "docker rm mysql_db django_cont nginx_cont || true"
+  // sh "docker stop mysql_db django_cont nginx_cont || true"
+  // sh "docker rm mysql_db django_cont nginx_cont || true"
 
   sh "docker compose down && docker compose up -d"
 }
