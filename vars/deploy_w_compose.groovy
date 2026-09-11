e2e94c04a50653f4df3c5fb039d3327e04cbd4ba
Abduldiyan Deshmukh <abduldiyandeshmukh2005@gmail.com>
2026-09-11 18:20:16 +0530

    Run docker compose in detached mode

--- vars/deploy_w_compose.groovy
+++ vars/deploy_w_compose.groovy
@@ -2,5 +2,5 @@ def call(){
   sh "docker stop mysql_db django_cont nginx_cont || true"
   sh "docker rm mysql_db django_cont nginx_cont || true"
 
-  sh "docker compose up"
+  sh "docker compose up -d"
 }
