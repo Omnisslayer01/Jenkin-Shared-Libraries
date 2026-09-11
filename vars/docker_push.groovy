4713b431d7d5e23c7c5fa16a7924a062e789f02c
Abduldiyan Deshmukh <abduldiyandeshmukh2005@gmail.com>
2026-09-12 00:06:32 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -5,7 +5,6 @@ def call(String image_name, String version){
     passwordVariable:'DockerHubPassword'
   )]){
   sh "docker login -u ${env.DockerHubUser} -p ${env.DockerHubPassword}"
-  sh "docker image tag notesapp ${env.DockerHubUser}/${image_name}:${version}"
   sh "docker push ${env.DockerHubUser}/${image_name}:${version}"
   }
 }
